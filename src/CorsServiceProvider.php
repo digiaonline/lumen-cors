40d6b4a52b8d929508a4daeaecc916ba1105a4c0
Christoffer Niska <christofferniska@gmail.com>
2016-03-07 23:26:57 +0200

    Refactor CorsServiceProvider

--- src/CorsServiceProvider.php
+++ src/CorsServiceProvider.php
@@ -1,23 +1,50 @@
 <?php namespace Nord\Lumen\Cors;
 
+use Illuminate\Config\Repository as ConfigRepository;
+use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\ServiceProvider;
+use Nord\Lumen\Cors\Contracts\CorsService as CorsServiceContract;
 
 class CorsServiceProvider extends ServiceProvider
 {
+
+    const CONFIG_KEY = 'cors';
 
     /**
      * @inheritdoc
      */
     public function register()
     {
-        $config = $this->app['config']['cors'];
+        $this->app->configure(self::CONFIG_KEY);
 
-        $this->app->bind('Nord\Lumen\Cors\CorsService', function () use ($config) {
-            return new CorsService($config);
+        $this->registerBindings($this->app, $this->app['config']);
+        $this->registerFacades();
+    }
+
+
+    /**
+     * Registers container bindings.
+     *
+     * @param Container        $container
+     * @param ConfigRepository $config
+     */
+    protected function registerBindings(Container $container, ConfigRepository $config)
+    {
+        $container->bind(CorsService::class, function () use ($config) {
+            return new CorsService($config[self::CONFIG_KEY]);
         });
 
-        $this->app->alias('Nord\Lumen\Cors\CorsService', 'Nord\Lumen\Cors\Contracts\CorsService');
+        $container->alias(CorsService::class, CorsServiceContract::class);
+    }
 
-        class_alias('Nord\Lumen\Cors\CorsFacade', 'Cors');
+
+    /**
+     * Registers facades.
+     */
+    protected function registerFacades()
+    {
+        if (!class_exists('Cors')) {
+            class_alias(CorsFacade::class, 'Cors');
+        }
     }
 }
