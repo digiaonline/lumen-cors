f1a1a32bb7e85e09c1b4556a0fc82706eed163c0
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2016-04-14 13:20:03 +0300

    Should fix codeception code coverage on travis...

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,8 @@
 <?php
-// This is global bootstrap for autoloading
+
+// Codeception Specify deep-clones everything by default...
+ini_set('xdebug.max_nesting_level', 1000);
+
+date_default_timezone_set('UTC');
+
+require __DIR__ . '/../vendor/autoload.php';
