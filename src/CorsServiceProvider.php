dcaab355fd948b19b6b4628b125a7877f3d12f04
Christoffer Niska <christofferniska@gmail.com>
2017-08-24 11:13:44 +0300

    Refactor CorsServiceProvider to be compatible with Laravel

--- src/CorsServiceProvider.php
+++ src/CorsServiceProvider.php
@@ -7,33 +7,24 @@ use Nord\Lumen\Cors\Contracts\CorsService as CorsServiceContract;
 
 class CorsServiceProvider extends ServiceProvider
 {
-    const CONFIG_KEY = 'cors';
-
     /**
      * @inheritdoc
      */
     public function register()
     {
-        $this->app->configure(self::CONFIG_KEY);
-
-        $this->registerBindings($this->app, $this->app['config']);
+        $this->registerBindings();
         $this->registerFacades();
     }
 
 
     /**
      * Registers container bindings.
-     *
-     * @param Container        $container
-     * @param ConfigRepository $config
      */
-    protected function registerBindings(Container $container, ConfigRepository $config)
+    protected function registerBindings()
     {
-        $container->bind(CorsService::class, function () use ($config) {
-            return new CorsService($config[self::CONFIG_KEY]);
+        $this->app->bind(CorsServiceContract::class, function () {
+            return new CorsService(config('cors'));
         });
-
-        $container->alias(CorsService::class, CorsServiceContract::class);
     }
 
 
